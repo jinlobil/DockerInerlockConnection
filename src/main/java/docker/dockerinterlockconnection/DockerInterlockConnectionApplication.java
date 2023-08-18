87c258291b6d6fe11f2265a2d671b5bcd92ef603
Hwang <jinlobil@gamil.com>
2023-08-18 17:33:00 +0900

    first commit

---
+++ src/main/java/docker/dockerinterlockconnection/DockerInterlockConnectionApplication.java
@@ -0,0 +1,13 @@
+package docker.dockerinterlockconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerInterlockConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerInterlockConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docker/dockerinterlockconnection/DockerInterlockConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package docker.dockerinterlockconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerInterlockConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
